861c3b831e45097147e5ad4339c8a7960c2707cd
PawanJaiswal0843 <pawankumarrajeshlal.j19@iiits.in>
2019-10-19 13:34:22 +0530

    created calculator

---
+++ calculator.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int sum(int a,int b)
+{
+     return a+b;
+}
+
+int main()
+{
+        int a=5,b=6;
+        printf("sum = %d",sum(a,b));
+	printf("diff = %d",diff(a,b));
+	return 0;
+}
