82f6e07ffd9fa37603ec97a49e4b5ea2cec457b4
PawanJaiswal0843 <pawankumarrajeshlal.j19@iiits.in>
2019-10-19 13:48:20 +0530

    edited product

--- calculator.c
+++ calculator.c
@@ -7,11 +7,16 @@ int diff(int a,int b)
 {
 	return a-b;
 }
+int product(int a,int b)
+{
+	return a*b;
+}
 
 int main()
 {
         int a=5,b=6;
         printf("sum = %d",sum(a,b));
 	printf("diff = %d",diff(a,b));
+	printf("product = %d",product(a,b));
 	return 0;
 }
